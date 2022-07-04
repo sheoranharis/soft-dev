175d99f52d24343610f2659c7bb4faaa09493f5a
sheoranharis <www.harishsheoran@gmail.com>
2022-07-04 13:06:30 +0530

    revision, change in js file

--- todo.js
+++ todo.js
@@ -7,38 +7,28 @@ let num = 0;
 while(mainAsk !== 'quit'){
     mainAsk = prompt("Select options- new | list | delete | quit")
     
-    while(mainAsk === 'new'){
+    if(mainAsk === 'new'){
         mainAsk = prompt("What would you like to add to your tasks??")
       
-        //for(let i =0; i < storage.length+1; i++){
-            task = `${num}: ${mainAsk}`
-        
-        num++;
         console.log(`${mainAsk}is added to tasks.`)
-        storage.push(task);
+        storage.push(mainAsk);
         
     }
-    if(mainAsk === 'list'){
+    else if(mainAsk === 'list'){
         console.log("********* Tasks **********")
-        for(let li of storage){
-            console.log(`      ${li}`)
+        for(let i=0; i<storage.length; i++){
+            console.log(`   ${i}: ${storage[i]}`)
         }
         console.log("****You are gonna do it****")
         //mainAsk = prompt("What would you like to do now")
-        continue;
+        
     }
     else if(mainAsk === 'delete'){
-        mainAsk = prompt('Which one do you like to delete? - Choose by number.')
-        if(mainAsk == '') {
-            break;
-        }
-        else{
-            storage.splice(`${mainAsk}`, 1)
-        }
-        continue;
+        mainAsk = parseInt(prompt('Which one do you like to delete? - Choose by number.'))
+        let deleted = storage.splice(`${mainAsk}`, 1)
+        console.log(`You deleted ${deleted}`)
+        
     }
-    
-
-
 
 }
+console.log("Ok, You quit the app.")
