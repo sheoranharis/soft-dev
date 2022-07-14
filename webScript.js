0a418d251807d10a6e7b3c356e7ae8a7b6ea302b
Harish Sheoran <www.harishsheoran@gmail.com>
2022-07-14 09:27:45 +0530

    auto generate bootstrap template script
    node webScript.js newFolderName

---
+++ webScript.js
@@ -0,0 +1,30 @@
+const fs = require("fs")
+
+let folder = process.argv[2] || "project"
+
+let index = `<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>New Project</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</head>
+
+<body>
+
+
+    <script src="app.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>`
+
+fs.mkdirSync(folder);
+fs.writeFileSync(`${folder}/index.html`, index)
+fs.writeFileSync(`${folder}/app.js`, "console.log('all set')")
+console.log("DONE")
